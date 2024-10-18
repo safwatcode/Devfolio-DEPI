e11873f565765c3e507a6d2eb2a6bba5e4e50808
Safwat Ahmed <safwatahmed669@gmail.com>
2024-10-18 17:27:03 +0300

    Finishing Routing

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -5,6 +5,11 @@ import { ContactComponent } from './components/contact/contact.component';
 
 export const routes: Routes = [
   {
+    path: '',
+    redirectTo: 'projects', // Redirect the base URL to the 'projects' route
+    pathMatch: 'full', // Ensures it only redirects when the full path is empty
+  },
+  {
     path: 'projects',
     component: ProjectsComponent,
   },
